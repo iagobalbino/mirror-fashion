7a41e824b7aa8421c4d6651be5ca381bb71eedb0
Iago Balbino <iago_nobre@hotmail.com>
2019-11-17 14:54:02 -0300

    Adicionado os campos Quantitade e Total

--- js/produto.js
+++ js/produto.js
@@ -2,7 +2,7 @@ var inputTamanho = document.querySelector('[name=tamanho]');
 var outputTamanho = document.querySelector('[name=valortamanho]');
 
 function mostraTamanho() {
-    outputTamanho = inputTamanho.value;
+    outputTamanho.value = inputTamanho.value;
 }
 
 inputTamanho.oninput = mostraTamanho;
