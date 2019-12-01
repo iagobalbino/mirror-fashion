17dd130ebcb6ea10785216792f2f19e47fd66056
Iago Balbino <iago_nobre@hotmail.com>
2019-11-30 22:29:52 -0300

    Adicionado JQuery a página checkout.html

--- js/home.js
+++ js/home.js
@@ -1,3 +1,11 @@
 $('.novidades button').click(function(){
     $('.novidades').addClass('painel-aberto');
 })
+
+$('.mais-vendidos button').click(function(){
+    $('.mais-vendidos').addClass('painel-aberto');
+})
+
+$('.painil button').click(function() {
+    $(this).parent().toggleClass('painel-aberto');
+})
